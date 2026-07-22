492b84368e7867f62a68fbeab807352b6a878f4b
Sahil Sawant <sahilsawant7120@gmail.com>
2026-07-22 13:52:44 +0530

    Implemented model trainin pipeline

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from cnnClassifier import logger
 from cnnClassifier.pipeline.stage_01_data_ingestion import DataIngestionPipeline
 from cnnClassifier.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
+from cnnClassifier.pipeline.stage_03_model_trainer import ModelTrainerPipeline
 
 STAGE_NAME = "Data Ingestion Stage"
 
@@ -23,3 +24,17 @@ try:
 except Exception as e:
     logger.exception(e)
     raise e
+
+
+STAGE_NAME = "Training"
+try:
+    logger.info(f"**************************************")
+    logger.info(f">>>>> stage {STAGE_NAME} started <<<<<")
+    model_trainer = ModelTrainerPipeline()
+    model_trainer.main()
+    logger.info(f">>>>> stage {STAGE_NAME} completed!<<<<<\n\nx==========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
+    
+    
---
+++ src/cnnClassifier/pipeline/stage_03_model_trainer.py
@@ -0,0 +1,45 @@
+from cnnClassifier.config.configuration import ConfigurationManager
+from cnnClassifier.components.model_trainer import Training
+from cnnClassifier.logger import logger
+
+STAGE_NAME = "Training"
+
+class ModelTrainerPipeline:
+    def __init__(self):
+        self.config = ConfigurationManager()
+        
+    def main(self):
+        def __init__(self):
+            pass
+        
+        def main(self):
+            config = ConfigurationManager()
+            training_config = config.get_training_config()
+            training = Training(config=training_config)
+            training.get_base_model()
+            training.train_valid_generator()
+            training.train()
+            
+            
+if __name__ == "__main__":
+    try:
+        logger.info(f"**************************************")
+        logger.info(f">>>>> stage {STAGE_NAME} started <<<<<")
+        obj = ModelTrainerPipeline()
+        obj.main()
+        logger.info(f">>>>> stage {STAGE_NAME} completed!<<<<<\n\nx==========x")
+        
+    except Exception as e:
+        logger.exception(e)
+        raise e
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
